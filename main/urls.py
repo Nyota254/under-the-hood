180a54fd941d6dbce74b67ccb8b25e10454156d8
Nyota254 <knyota66@gmail.com>
2019-10-31 15:23:50 +0300

    add functionality for uploading car types

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import re_path
 from .views import (
     index_view,
+    car_type_upload,
 )
 
 urlpatterns = [
-    re_path('^$',index_view,name='index_view')
+    re_path('^$',index_view,name='index_view'),
+    re_path('^UploadCarType/$',car_type_upload,name='upload-car-type')
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import (
+    Car_Type_Form,
+    Car_Upload_Form,
+    Car_Model_Form,
+    Part_Upload_Form,
+    Problem_Upload
+)
 
 def index_view(request):
     '''
@@ -8,3 +15,22 @@ def index_view(request):
         "title":"Home"
     }
     return render(request,'main/index.html',context)
+
+
+def car_type_upload(request):
+    '''
+    This view function will help in uploading of a car type
+    '''
+    if request.method == 'POST':
+        form = Car_Type_Form(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(car_type_upload)
+    else:
+        form = Car_Type_Form()
+    
+    context = {
+        "title":"upload car type",
+        "form":form
+    }
+    return render(request,"main/car_type_upload_form.html",context)
