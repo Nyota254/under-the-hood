fc330ac0a2f73f565b5a0bb4cf0706298e0e13dc
Nyota254 <knyota66@gmail.com>
2019-10-31 14:43:43 +0300

    create the forms for the application

---
+++ main/forms.py
@@ -0,0 +1,53 @@
+from django import forms
+from .models import (
+    Car_Type,
+    Car_Model,
+    Car,
+    Part,
+    Problem,
+)
+
+class Car_Type_Form(forms.ModelForm):
+    '''
+    Form for uploading the car type from (Toyota,Subs)
+    '''
+    class Meta:
+        model = Car_Type
+        fields = ('car_type_name','description')
+
+class Car_Model_Form(form.ModelForm):
+    '''
+    Form for uploading the car model from a car type
+    '''
+    class Meta:
+        model = Car_Model
+        fields = ('car_model_image','car_model_name','version','date_realised')
+
+class Car_Upload_Form(form.ModelForm):
+    '''
+    Form for uploading the users car
+    '''
+    class Meta:
+        model = Car
+        fields = ('registration_number','car_type','model','mileage','date_bought')
+
+class Part_Upload_Form(form.ModelForm):
+    '''
+    Form for uploading car parts
+    '''
+    class Meta:
+        model = Part
+        fields =('part_image','part_name')
+
+class Problem_Upload(form.ModelForm):
+    '''
+    Form for uploading a car problem
+    '''
+    part = forms.ModelChoiceField(queryset=part.objects.all())
+    class Meta:
+        model = Problem
+        fields = ('problem_name','problem_description','part','fix','car','notes')
+
+     def __init__(self, user, *args, **kwargs):
+        super(Problem_Upload, self).__init__(*args, **kwargs)
+        self.fields['car'].queryset = Car.objects.filter(owner=user)
