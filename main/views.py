e3052343cb7f100c9f4e48c056e1739e44c9ee78
Nyota254 <knyota66@gmail.com>
2019-10-31 08:47:54 +0300

    setup the view function and index html for redirection after login

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index_view(request):
+    '''
+    Will render the home page
+    '''
+    context = {
+        "title":"Home"
+    }
+    return render(request,'main/index.html',context)
