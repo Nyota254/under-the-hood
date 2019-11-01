cf8cffebdc6429e5c97d3f8ba308bc95b0d241af
Nyota254 <knyota66@gmail.com>
2019-11-01 07:26:26 +0300

    add url for car model upload

--- main/urls.py
+++ main/urls.py
@@ -2,9 +2,11 @@ from django.urls import re_path
 from .views import (
     index_view,
     car_type_upload,
+    car_model_addition,
 )
 
 urlpatterns = [
     re_path('^$',index_view,name='index_view'),
-    re_path('^UploadCarType/$',car_type_upload,name='upload-car-type')
+    re_path('^UploadCarType/$',car_type_upload,name='upload-car-type'),
+    re_path('^UploadCarModel/$',car_model_addition,name='upload-car-model')
 ]
