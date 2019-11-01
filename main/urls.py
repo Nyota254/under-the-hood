0ba79c5053244332871569456f6738518c32ce5f
Nyota254 <knyota66@gmail.com>
2019-11-01 07:49:38 +0300

    create urls for data upload forms

--- main/urls.py
+++ main/urls.py
@@ -3,10 +3,16 @@ from .views import (
     index_view,
     car_type_upload,
     car_model_addition,
+    car_parts_upload,
+    car_upload,
+    car_problem_upload,
 )
 
 urlpatterns = [
     re_path('^$',index_view,name='index_view'),
     re_path('^UploadCarType/$',car_type_upload,name='upload-car-type'),
-    re_path('^UploadCarModel/$',car_model_addition,name='upload-car-model')
+    re_path('^UploadCarModel/$',car_model_addition,name="upload-car-model"),
+    re_path('^CarPartUpload/$',car_parts_upload,name="upload-car-part"),
+    re_path('^CarUpload/$',car_upload,name="car-upload"),
+    re_path('^CarProplemUpload/$',car_problem_upload,name="car-problem-upload")
 ]
